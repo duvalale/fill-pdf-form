08c9de2b014ab2e2ce244e0e16307d2b0cd53510
Alexandra Duval <alexandra.duval@orange.com>
2025-01-23 18:40:32 +0100

    Fix iot_class and config_schema validation issues

--- custom_components/fill_pdf_form/__init__.py
+++ custom_components/fill_pdf_form/__init__.py
@@ -3,16 +3,18 @@
 import logging
 import os
 
+from homeassistant.helpers import config_validation as cv
 from PyPDF2 import PdfReader, PdfWriter
 
 DOMAIN = "fill_pdf_form"
+
+CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
 
 INPUT_FILE_PATH_NAME = "input_file_path"
 OUTPUT_FILE_PATH_NAME = "output_file_path"
 FIELDS_DATA_NAME = "fields_data"
 
 _LOGGER = logging.getLogger(__name__)
-
 
 def setup(hass, config):
     """Set up is called when Home Assistant is loading our component."""
@@ -38,11 +40,12 @@ def setup(hass, config):
             page = reader.pages[0]
             fields = reader.get_fields()
 
-            _LOGGER.info('input fields', updated_fields)
-            _LOGGER.info('fields from form', fields)
+            _LOGGER.info(updated_fields)
+            _LOGGER.info(fields)
 
-            ## Fill fields and save file ##
+            ## Fill fields and flatten file ##
             writer.add_page(page)
+
             writer.update_page_form_field_values(writer.pages[0], updated_fields)
 
             with open(full_output_file_path, "wb") as output_stream:
